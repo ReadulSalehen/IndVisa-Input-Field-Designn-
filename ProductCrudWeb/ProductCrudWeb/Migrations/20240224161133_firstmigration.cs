b4567119807e543ac0da226c64faf9812f0cbbaa
Readul Salehen <readul7214@gmail.com>
2024-02-25 13:06:16 +0600

    initial

---
+++ ProductCrudWeb/ProductCrudWeb/Migrations/20240224161133_firstmigration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductCrudWeb.Migrations
+{
+    public partial class firstmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Country = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    IndianMissionOffice = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Nationality = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DateofBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ExpectedDateOfArivel = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    VisaType = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Purpose = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ ProductCrudWeb/ProductCrudWeb/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductCrudWeb.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [MinLength(2, ErrorMessage = "Country name should be more than 2 characters")]
+        public string Country { get; set; } = string.Empty;
+        [Required]
+        [StringLength(50)]
+        [MinLength(2, ErrorMessage = "Indian Mission Office name should be more than 2 characters")]
+        public string IndianMissionOffice { get; set; } = string.Empty;
+        [Required]
+        [StringLength(50)]
+        [MinLength(2, ErrorMessage = "Nationality name should be more than 2 characters")]
+        public string Nationality { get; set; } = string.Empty;
+        [Required]
+        public DateTime? DateofBirth { get; set; }
+        [Required]
+        [StringLength(100)]
+        [MinLength(6, ErrorMessage = "Email name should be more than 6 characters")]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public DateTime? ExpectedDateOfArivel { get; set; }
+        [Required]
+        [StringLength(50)]
+        [MinLength(2, ErrorMessage = "Visa Type name should be more than 2 characters")]
+        public string VisaType { get; set; } = string.Empty;
+        [Required]
+        [StringLength(50)]
+        [MinLength(2, ErrorMessage = "Purpose name should be more than 2 characters")]
+        public string Purpose { get; set; } = string.Empty;
+    }
+}
